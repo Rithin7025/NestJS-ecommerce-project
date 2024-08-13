b72d1828f18eb25f092c4a665be150ad3c4b90ed
unknown <rithinrj2001@gmail.com>
2024-08-13 16:48:35 +0530

    add controller for product management

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post,Delete, Put, Body, Param, Query, NotFoundException } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDTO } from './dtos/create-product.dto.ts';
+import { FilterProductDTO } from './dtos/filter-product.dto.ts';
+@Controller('store/products')   //* set the part of url shared by all endpoints
+export class ProductController {
+    constructor(private productService : ProductService) {} //? injecting the product service 
 
-@Controller('product')
-export class ProductController {}
+    @Get('/')
+    async getProducts(@Query() filterProductDTO : FilterProductDTO) {
+        if(Object.keys(filterProductDTO).length){
+            const filteredProducts = await this.productService.getfilteredProducts(filterProductDTO)
+            return filteredProducts; 
+        }else {
+            const allProducts  =  await this.productService.getAllProducts();
+            return allProducts;
+        }
+    }
+
+    @Get('/:id')
+    async getProduct(@Param('id') id: string){
+        const product = await this.productService.getProduct(id);
+        if(!product) throw new NotFoundException('Product does not Exist!!');
+        return product;
+    }
+
+    @Post('/')
+    async addProduct(@Body() createProductDTO : CreateProductDTO) {
+        const product  = await this.productService.addProduct(createProductDTO);
+        return product;
+    }
+
+    @Put('/:id')
+    async updateProduct(@Param('id') id : string, @Body() createProductDTO : CreateProductDTO){
+        const product = await this.updateProduct(id, createProductDTO);
+        if(!product) throw new NotFoundException('Product does not exist');
+        return product;
+    }
+
+    @Delete('/:id')
+    async deleteproduct(@Param('id') id : string ){
+        const product = await this.productService.deleteProduct(id);
+        if(!product) throw new NotFoundException('Product does not exists');
+        return product;
+    }
+}
+
+
