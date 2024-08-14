c2bac273c2329d8a493fa0453914ea77feb32514
unknown <rithinrj2001@gmail.com>
2024-08-14 10:04:50 +0530

    add user DTO

--- src/user/dtos/create-user-dto.ts
+++ src/user/dtos/create-user-dto.ts
@@ -1,7 +1,14 @@
+import { IsString, IsEmail } from "class-validator";
+
 export class CreateUserDTO{
+
+    @IsString()
     userName : string;
+    @IsEmail()
     email  : string ; 
+    @IsString()
     password : string ; 
+    
     roles : string[];
 }
 
